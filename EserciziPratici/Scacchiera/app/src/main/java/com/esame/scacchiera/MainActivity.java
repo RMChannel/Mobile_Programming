22cc54a524856f970900adeb2e6f671f4f7359f7
Roberto Cito <rmchannel82@gmail.com>
2026-02-16 23:14:41 +0100

    Scacchiera completato

--- EserciziPratici/Scacchiera/app/src/main/java/com/esame/scacchiera/MainActivity.java
+++ EserciziPratici/Scacchiera/app/src/main/java/com/esame/scacchiera/MainActivity.java
@@ -1,15 +1,31 @@
 package com.esame.scacchiera;
 
+import static android.widget.Toast.LENGTH_SHORT;
+
+import android.annotation.SuppressLint;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
+public class MainActivity extends AppCompatActivity {
+    private Mappa mappa;
+    private ArrayList<Integer> mappaID;
+    private int count;
+    private boolean victory;
+
+    @SuppressLint("NewApi")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +36,127 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        mappa=new Mappa();
+        mappaID=new ArrayList<>();
+        count=0;
+        victory=false;
+
+        android.widget.TableLayout grid = findViewById(R.id.mainGrid);
+        for(int i=0; i<grid.getChildCount(); i++) {
+            android.view.View child = grid.getChildAt(i);
+            if(child instanceof android.widget.TableRow) {
+                android.widget.TableRow row = (android.widget.TableRow) child;
+                for(int j=0; j<row.getChildCount(); j++) {
+                    android.view.View cell = row.getChildAt(j);
+                    if(cell instanceof android.widget.TextView) {
+                        if (cell.getId() == android.view.View.NO_ID) {
+                            cell.setId(android.view.View.generateViewId());
+                        }
+                        cell.setTag(i + "," + j);
+                        mappaID.add(cell.getId());
+                    }
+                }
+            }
+        }
+
+        stampaMappa();
+    }
+
+    public void stampaMappa() {
+        for(int i=0;i<3;i++) {
+            for(int j=0;j<3;j++) {
+                TextView text=findViewById(mappaID.get(i*3+j));
+                text.setText(String.valueOf((char)mappa.get(i,j)));
+            }
+        }
+        TextView text=findViewById(R.id.displayValue);
+        text.setText(String.valueOf(count));
+        Log.d("MAPPA",mappa.toString());
+    }
+
+    public void moveX(View view) {
+        if(victory) {
+            Toast.makeText(this,"Hai vinto!!!",LENGTH_SHORT).show();
+            return;
+        }
+        String direction=view.getTag().toString();
+        int x=0;
+        if(direction.contains("2")) x=1;
+        else if(direction.contains("3")) x=2;
+        if(direction.contains("left")) {
+            mappa.moveX(x,0);
+        }
+        else if(direction.contains("right")) {
+            mappa.moveX(x,1);
+        }
+        count++;
+        stampaMappa();
+        if(mappa.hasWon()) mostraVittoria();
+    }
+
+    public void moveY(View view) {
+        if(victory) {
+            Toast.makeText(this,"Hai vinto!!!",LENGTH_SHORT).show();
+            return;
+        }
+        String direction=view.getTag().toString();
+        int y=0;
+        if(direction.contains("2")) y=1;
+        else if(direction.contains("3")) y=2;
+        if(direction.contains("up")) {
+            mappa.moveY(y,0);
+        }
+        else if(direction.contains("down")) {
+            mappa.moveY(y,1);
+        }
+        count++;
+        stampaMappa();
+        if(mappa.hasWon()) mostraVittoria();
+    }
+
+    private void mostraVittoria() {
+        victory=true;
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Complimenti! 🎉");
+        builder.setMessage("Hai vinto la partita! Ottimo lavoro.");
+
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+
+        builder.setCancelable(false);
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+    public void reset(View view) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        builder.setTitle("Conferma reset");
+        builder.setMessage("Sei sicuro di voler riavviare la partita??");
+
+        builder.setPositiveButton("Sì", new DialogInterface.OnClickListener() {
+            @SuppressLint("NewApi")
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                mappa=new Mappa();
+                count=0;
+                victory=false;
+                stampaMappa();
+            }
+        });
+        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 }
--- EserciziPratici/Scacchiera/app/src/main/java/com/esame/scacchiera/Mappa.java
+++ EserciziPratici/Scacchiera/app/src/main/java/com/esame/scacchiera/Mappa.java
@@ -1,4 +1,103 @@
 package com.esame.scacchiera;
 
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
+
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Mappa {
+    private final ArrayList<ArrayList<Integer>> mappa;
+
+    @RequiresApi(api = Build.VERSION_CODES.VANILLA_ICE_CREAM)
+    public Mappa() {
+        this.mappa=new ArrayList<>();
+        for(int i=0;i<3;i++) {
+            ArrayList<Integer> temp=new ArrayList<>();
+            for(int j=0;j<3;j++) temp.add(0);
+            mappa.add(temp);
+        }
+        randomGenerate();
+    }
+
+    public void set(int x, int y, int value) {
+        mappa.get(x).set(y,value);
+    }
+
+    public int get(int x, int y) {
+        return mappa.get(x).get(y);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.VANILLA_ICE_CREAM)
+    private void randomGenerate() {
+        String base="123456789";
+        Random random=new Random();
+        int i=0;
+        int j=0;
+        while(!base.isEmpty()) {
+            int r=random.nextInt(0,base.length());
+            set(i,j, (int) base.charAt(r));
+            base = base.substring(0, r) + base.substring(r + 1);
+            j++;
+            if(j==3) {
+                i+=1;
+                j=0;
+            }
+        }
+    }
+
+    public void moveX(int x, int direction) {
+        ArrayList<Integer> temp=mappa.get(x);
+        ArrayList<Integer> temp2=new ArrayList<>();
+        if(direction==1) { //right
+            temp2.add(temp.get(2));
+            temp2.add(temp.get(0));
+            temp2.add(temp.get(1));
+        }
+        else { //left
+            temp2.add(temp.get(1));
+            temp2.add(temp.get(2));
+            temp2.add(temp.get(0));
+        }
+        mappa.set(x,temp2);
+    }
+
+    public void moveY(int y, int direction) {
+        int y0=mappa.get(0).get(y);
+        int y1=mappa.get(1).get(y);
+        int y2=mappa.get(2).get(y);
+        if(direction==1) {
+            mappa.get(0).set(y,y2);
+            mappa.get(1).set(y,y0);
+            mappa.get(2).set(y,y1);
+        }
+        else {
+            mappa.get(0).set(y,y1);
+            mappa.get(1).set(y,y2);
+            mappa.get(2).set(y,y0);
+        }
+    }
+
+    public boolean hasWon() {
+        int old=0;
+        for(int i=0;i<3;i++) {
+            for(int j=0;j<3;j++) {
+                if(old>mappa.get(i).get(j)) return false;
+                else old=mappa.get(i).get(j);
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb=new StringBuilder();
+        for(int i=0;i<3;i++) {
+            for(int j=0;j<3;j++) {
+                sb.append(i).append(",").append(j).append("=").append(get(i, j));
+            }
+        }
+        return sb.toString();
+    }
 }
